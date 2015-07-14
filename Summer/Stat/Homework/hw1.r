6b4c4426317c7a3ba1dba22b21779a4075bbd96f
Marcus Rosti <mer3ef@virginia.edu>
2015-07-14 14:31:05 -0400

    Finished CS class and Updated homework for stat

--- Summer/Stat/Homework/hw1.r
+++ Summer/Stat/Homework/hw1.r
@@ -10,10 +10,11 @@ setwd("~/MSDS/Summer/Stat/Homework")
 grade.data <- read.csv("samplegrades.csv", header=TRUE)
 summary(grade.data) # Just looking at the summary 
 
+### 1 ###
 ## a
 # count the number of students who failed the class
-length(grade.data$Student.ID[grade.data$Course.Average..100.0.<60])
-# 24
+length(grade.data$Student.ID[grade.data$Course.Average..100.0.<60])/5.66
+# ~4.24
 
 ## b
 sum(grade.data$Final.Exam..100.0.==0)
@@ -24,20 +25,50 @@ final_mean <- mean(grade.data$Final.Exam..100.0.)
 quiz_mean  <- mean(grade.data$Quiz..45.0.)/45*100
 mid_mean   <- mean(grade.data$Midterm..100.0.)
 max_mean   <- max(final_mean,quiz_mean,mid_mean)
+
+# Some logic to print out the right answer
 if (final_mean==max_mean) {
-  print("The final was highest")
+    print("The final was highest")
 } else if (quiz_mean==max_mean) {
-  print("The quizes were highest")
+    print("The quizes were highest")
 } else {
-  print("The midterm was highest")
+    print("The midterm was highest")
 }
 # Quizes had the highest
 
 ## d
+# The number of people that had trouble on quizes but got at least a B on the midterm
 sum(grade.data$Midterm..100.0.>=80)
 #232
 sum(grade.data$Quiz..45.0.<=70*45/70)
 #566
 sum(grade.data$Quiz..45.0.<=(70*45/100) & grade.data$Midterm..100.0.>=80)
-#8
- 
+# 8 people
+
+## e
+# The number of people who got a lower course grade than their final exam
+sum(grade.data$Course.Average..100.0. < grade.data$Final.Exam..100.0.) / 5.66
+# ~8.12
+
+## f
+# Make subsets of the data
+top_20 <- subset(grade.data, grade.data$Course.Average..100.0>quantile(grade.data$Course.Average..100.0.,.8))
+bottom_20 <- subset(grade.data, grade.data$Course.Average..100.0.< quantile(grade.data$Course.Average..100.0.,.2))
+
+sum(top_20$Course.Average..100.0. < top_20$Final.Exam..100.0.) / 1.13
+# 9.73
+sum(bottom_20$Course.Average..100.0. < bottom_20$Final.Exam..100.0.) / 1.13
+# 13.2
+
+# g
+# #of students with quiz grades between 70 and 80
+sum(grade.data$Quiz..45.0.>70*45/100 & grade.data$Quiz..45.0.<80*45/100)
+
+# #of students with homework grades between 90  and 95
+sum(grade.data$Homework..200.0./2>90 & grade.data$Homework..200.0./2<95)
+
+# #in both groups
+sum( (grade.data$Quiz..45.0.>70*45/100 & grade.data$Quiz..45.0.<80*45/100) | (grade.data$Homework..200.0./2>90 & grade.data$Homework..200.0./2<95) )
+#111
+
+### 2 ###
